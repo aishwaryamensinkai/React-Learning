cc7c4954e7f89dc3dcc08923f4d8db618b17d0cf
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-02-25 19:05:38 -0500

    Typescript | React Type Safety

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Person,Country} from './Person';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+    TypeScript | React Type Safety
+        <Person
+        name="Pedro"
+        email="pedro@gmail.com"
+        age={21}
+        isMarried={true}
+        friends={["jessica", "jake", "jerry", "jasmine"]}
+        country={ Country.India}
+        />
+      </div>
   );
 }
 
