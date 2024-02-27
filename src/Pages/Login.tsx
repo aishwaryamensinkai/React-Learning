ad1ba5f509a14dae02fc089d091e83d1181a0bed
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-02-26 23:51:13 -0500

    Redux Toolkit

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import { login, logout } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+
 export const Login = () => {
-  return <div className="App">
-    <h1>Login Page</h1>
-    <br />
-    <input />{"  "}<button>Login</button>{"  "}<button>Logout</button>
-  </div>
-}
+  const [newUsername, setNewUsername] = useState<string>("");
+
+  const dispatch = useDispatch();
+  const username = useSelector((state: any) => state.user.value.username);
+
+  return (
+    <h1>
+      {username}
+      <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setNewUsername(e.target.value);
+        }}
+      />
+      <button onClick={() => dispatch(login({ username: newUsername }))}>
+        Submit Login
+      </button>
+      <button onClick={() => dispatch(logout())}> Logout </button>
+    </h1>
+  );
+};
